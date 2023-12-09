272aa354220058741156ce7583fe54ea86c491a7
Anton Terentev <gamervii@icloud.com>
2023-12-09 16:39:11 +0300

    clear project

--- GamerVII.Launcher/Models/Users/IUser.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GamerVII.Launcher.Models.Users;
-public interface IUser
-{
-    string Login { get; set; }
-    string Password { get; set; }
-    bool IsLogin { get; set; }
-    string AccessToken { get; set; }
-}
--- GamerVII.Launcher/ViewModels/Base/PageViewModelBase.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GamerVII.Launcher.ViewModels.Base;
-
-/// <summary>
-/// Base class for view models related to pages.
-/// </summary>
-public abstract class PageViewModelBase : ViewModelBase
-{
-    
-}
