cd52aefe832b806e41f3f2cf081b2435b71baa68
Anton Terentev <gamervii@icloud.com>
2024-05-02 13:21:04 +0300

    Add batch file for cloning repositories
    A batch file called "load-repositories.bat" has been added to automate the cloning of GamerVII.Notification.Avalonia and Gml.Client repositories. Also, the batch file has been included in Gml.Launcher solution for easy access.

---
+++ load-repositories.bat
@@ -0,0 +1,5 @@
+# clone GamerVII.Notification.Avalonia
+git clone --recursive https://github.com/GamerVII-NET/GamerVII.Notification.Avalonia.git ./src/GamerVII.Notification.Avalonia
+
+# clone Gml.Client
+git clone https://github.com/GamerVII-NET/Gml.Client.git ./src/Gml.Client
