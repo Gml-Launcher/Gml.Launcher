b5cb9abba54e46ebe66dc5609180e025ee431a7d
GamerVII-NET <111225722+GamerVII-NET@users.noreply.github.com>
2024-07-13 10:21:28 +0300

    Switch ProfileUserControl to ItemsControl and add Spacing
    Updated ProfileUserControl from TemplatedControl to ItemsControl to support dynamic item presentations. Introduced a new Spacing property to allow custom spacing between items. Additionally, moved ProfileInfoComponent and GmlButton from the UserControl template to the ProfilePageView for better separation of concerns.

--- src/Gml.Launcher/Views/Components/ProfileUserControl.axaml.cs
+++ src/Gml.Launcher/Views/Components/ProfileUserControl.axaml.cs
@@ -6,18 +6,22 @@ using Gml.Launcher.Models;
 
 namespace Gml.Launcher.Views.Components;
 
-public class ProfileUserControl : TemplatedControl
+public class ProfileUserControl : ItemsControl
 {
+    public static readonly StyledProperty<int> SpacingProperty = AvaloniaProperty.Register<StackFrameBorder, int>(
+        nameof(Spacing));
 
     public static readonly StyledProperty<IEnumerable<ProfileInfoItem>> ProfileInfoItemsProperty =
         AvaloniaProperty.Register<ProfileUserControl, IEnumerable<ProfileInfoItem>>(
             nameof(ProfileInfoItems));
 
-    public static readonly StyledProperty<string> SkinUrlProperty = AvaloniaProperty.Register<ProfileUserControl, string>(
-        nameof(SkinUrl), "https://www.clipartkey.com/mpngs/m/215-2156859_fond-transparent-steve-minecraft.png");
+    public static readonly StyledProperty<string> SkinUrlProperty =
+        AvaloniaProperty.Register<ProfileUserControl, string>(
+            nameof(SkinUrl), "https://www.clipartkey.com/mpngs/m/215-2156859_fond-transparent-steve-minecraft.png");
 
-    public static readonly StyledProperty<string> UserNameProperty = AvaloniaProperty.Register<ProfileUserControl, string>(
-        nameof(UserName), "GamerVII");
+    public static readonly StyledProperty<string> UserNameProperty =
+        AvaloniaProperty.Register<ProfileUserControl, string>(
+            nameof(UserName), "GamerVII");
 
     public string UserName
     {
@@ -37,4 +41,9 @@ public class ProfileUserControl : TemplatedControl
         set => SetValue(ProfileInfoItemsProperty, value);
     }
 
+    public int Spacing
+    {
+        get => GetValue(SpacingProperty);
+        set => SetValue(SpacingProperty, value);
+    }
 }
