1977b19c103749b51963c0d219dd63edb2a38a34
GamerVII-NET <111225722+GamerVII-NET@users.noreply.github.com>
2024-07-13 10:21:36 +0300

    Add URL redirect attached property
    Introduce `UrlRedirectProperty` to handle URL redirections in controls. This property changes start a process to open the URL with the default system browser, enhancing the app's interactivity.

---
+++ src/Gml.Launcher/Core/Converters/UrlRedirectProperty.cs
@@ -0,0 +1,30 @@
+using System.Diagnostics;
+using Avalonia;
+using Avalonia.Controls;
+
+namespace Gml.Launcher.Core.Converters;
+
+public class UrlRedirectProperty
+{
+    public static readonly AttachedProperty<string> RedirectUrlProperty =
+        AvaloniaProperty.RegisterAttached<UrlRedirectProperty, Control, string>("RedirectUrl");
+
+    public static void SetRedirectUrl(Control obj, string value) => obj.SetValue(RedirectUrlProperty, value);
+    public static string GetRedirectUrl(Control obj) => obj.GetValue(RedirectUrlProperty);
+
+    public UrlRedirectProperty()
+    {
+        RedirectUrlProperty.Changed.AddClassHandler<Control>(OnSourceChanged);
+    }
+
+    private void OnSourceChanged(Control control, AvaloniaPropertyChangedEventArgs args)
+    {
+        var url = args.GetNewValue<string>();
+
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = url,
+            UseShellExecute = true
+        });
+    }
+}
