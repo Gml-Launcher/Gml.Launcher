95606f905d3abee230c7d7df43f1692abb333864
terentev.a.a <terentev.a.a@recloud.tech>
2024-11-25 10:00:01 +0300

    Enable recursive clone for Gml.Client repository
    Added the --recursive option to the Gml.Client repository clone command to ensure all submodules are fetched during the clone process. This change is crucial for ensuring any dependencies within submodules are also cloned, preventing potential build issues.

--- load-repositories.bat
+++ load-repositories.bat
@@ -2,4 +2,4 @@
 git clone --recursive https://github.com/GamerVII-NET/GamerVII.Notification.Avalonia.git ./src/GamerVII.Notification.Avalonia
 
 
-git clone https://github.com/GamerVII-NET/Gml.Client.git ./src/Gml.Client
+git clone --recursive https://github.com/GamerVII-NET/Gml.Client.git ./src/Gml.Client
