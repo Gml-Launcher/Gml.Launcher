892853e4772a85fa51ca15ab680536f977ff0ec8
terentev.a.a <terentev.a.a@recloud.tech>
2024-11-26 04:36:09 +0300

    Add Chinese language support
    Introduced Chinese localization by adding a new language resource file. Updated the Language model and SystemService to include the new language option and modified the SettingsPageView to temporarily comment out unused text elements.

--- src/Gml.Launcher/Models/Language.cs
+++ src/Gml.Launcher/Models/Language.cs
@@ -1,9 +1,22 @@
-﻿using System.Globalization;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 
 namespace Gml.Launcher.Models;
 
 public class Language
 {
+    public Language()
+    {
+    }
+
+    [SetsRequiredMembers]
+    public Language(string iconPath, string name, CultureInfo culture)
+    {
+        IconPath = iconPath;
+        Name = name;
+        Culture = culture;
+    }
+
     public required string IconPath { get; init; }
     public required string Name { get; init; }
     public required CultureInfo Culture { get; init; }
