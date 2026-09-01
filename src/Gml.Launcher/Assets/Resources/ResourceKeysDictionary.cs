a551b95aef88257ca55f239d6dfa26180dbd6fd7
awktpk <animeworld3031@gmail.com>
2026-09-01 16:55:01 +0300

    Fix conflixt 2
    Второй фикс

---
+++ src/Gml.Launcher/Assets/Resources/ResourceKeysDictionary.cs
@@ -0,0 +1,8 @@
+﻿namespace Gml.Launcher.Assets;
+
+public static class ResourceKeysDictionary
+{
+    public const string Host = "https://gmlf.recloud.tech";
+    public const string SecondaryHost = "http://localhost:5002";
+    public const string FolderName = "GmlCraft";
+}
